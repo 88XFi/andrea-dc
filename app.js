850796e10e4d2fdc6f01f5f1727f8a5e0e6f5413
slify08 <islimaxmc@gmail.com>
2021-03-08 10:14:54 +0800

    update

--- app.js
+++ app.js
@@ -22,3 +22,15 @@ let tab = $('.tab').hide();
 $('.menu').click(() => {
     $('.tab').slideToggle(1000);
 });
+
+function changeSize() {
+    if (window.matchMedia('(max-width: 1100px)').matches) {
+        $('.fb-post').attr('data-width', '350px');
+    } else {
+        $('.fb-post').attr('data-width', '500px');
+    }
+}
+
+let x = window.matchMedia('(max-width: 1100px)');
+
+changeSize();
